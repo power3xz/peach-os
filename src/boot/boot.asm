5a3fb51f3413516cf3cdc6159121c32982808cf0
devj <power3xz@gmail.com>
2025-02-12 14:43:26 +0900

    kernel.asm 추가

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -28,7 +28,8 @@ step2:
   mov eax, cr0
   or eax, 0x1
   mov cr0, eax
-  jmp CODE_SEG:load32
+  ; jmp CODE_SEG:load32
+  jmp $
 
 ; GDT
 gdt_start:
@@ -58,24 +59,5 @@ gdt_descriptor:
   dw gdt_end - gdt_start - 1
   dd gdt_start
 
-
-[BITS 32]
-load32:
-  mov ax, DATA_SEG
-  mov ds, ax
-  mov es, ax
-  mov fs, ax
-  mov gs, ax
-  mov ss, ax
-  mov ebp, 0x00200000
-  mov esp, ebp
-
-  ; enable A20 line
-  in al, 0x92
-  or al, 2
-  out 0x92, al
-
-  jmp $
 times 510 - ($ - $$) db 0 ; 510 바이트까지 0으로 채움
 dw 0xaa55 ; 부트 섹터 마지막에 0xaa55를 삽입하여 부트 섹터임을 표시
-
