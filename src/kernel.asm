911353571a5d40a5e2421b0bd8bd5e3450ba9874
devj <power3xz@gmail.com>
2025-02-12 22:47:51 +0900

    kernel.c 빌드 추가

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,8 +1,7 @@
 [BITS 32]
 
-section .asm
-
 global _start
+extern kernel_main
 
 CODE_SEG equ 0x08
 DATA_SEG equ 0x10
@@ -22,6 +21,6 @@ _start:
   or al, 2
   out 0x92, al
 
-  jmp $
+  jmp kernel_main
 
 times 512 - ($ - $$) db 0 ; 512 바이트까지 0으로 채움
