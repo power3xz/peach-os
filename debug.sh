d598f201ea9e656bc4a7750d26e416da3e8fc590
devj <power3xz@gmail.com>
2025-02-12 22:11:17 +0900

    debug.sh 수정

--- debug.sh
+++ debug.sh
@@ -1,2 +1,1 @@
-# run qemu in background and wait for gdb to connect
-qemu-system-x86_64 -hda ./bin/boot.bin -s -S & lldb -o "gdb-remote localhost:1234" -o "break *0x7c00" -o "continue"
+x86_64-elf-gdb -x ./gdb-commands.txt
