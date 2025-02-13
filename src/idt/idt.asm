2e48e1e3a51cb6d62cb5a553cdbcddd9e9f033ff
devj <power3xz@gmail.com>
2025-02-13 15:58:24 +0900

    interrupt handler 추가

--- src/idt/idt.asm
+++ src/idt/idt.asm
@@ -1,6 +1,11 @@
 section .asm
 
+extern init21h_handler
+extern no_interrupt_handler
+
 global idt_load
+global init21h
+global no_interrupt
 
 idt_load:
   push ebp
@@ -11,3 +16,23 @@ idt_load:
 
   pop ebp
   ret
+
+init21h:
+  cli
+  pushad
+
+  call init21h_handler
+
+  popad
+  sti
+  iret
+
+no_interrupt:
+  cli
+  pushad
+
+  call no_interrupt_handler
+
+  popad
+  sti
+  iret
--- src/idt/idt.c
+++ src/idt/idt.c
@@ -2,11 +2,25 @@
 #include "config.h"
 #include "memory/memory.h"
 #include "kernel.h"
+#include "io/io.h"
 
 struct idt_desc idt_descriptors[PEACHOS_TOTAL_INTERRUPTS];
 struct idtr_desc idtr_descriptor;
 
 extern void idt_load(void *ptr);
+extern void init21h();
+extern void no_interrupt();
+
+void init21h_handler()
+{
+  print("keyword pressed");
+  outb(0x20, 0x20);
+}
+
+void no_interrupt_handler()
+{
+  outb(0x20, 0x20);
+}
 
 void idt_zero()
 {
@@ -29,7 +43,12 @@ void idt_init()
   idtr_descriptor.limit = sizeof(idt_descriptors) - 1;
   idtr_descriptor.base = (uint32_t)idt_descriptors;
 
+  for (int i = 0; i < PEACHOS_TOTAL_INTERRUPTS; i++)
+  {
+    idt_set(i, no_interrupt);
+  }
   idt_set(0, idt_zero);
+  idt_set(0x21, init21h);
 
   // load the interrupt descriptor table
   idt_load(&idtr_descriptor);
