9bca06a817ff03375f89d51d560fdfaf74862bca
devj <power3xz@gmail.com>
2025-02-14 11:08:23 +0900

    메모리 해제 추가

--- src/memory/heap.c
+++ src/memory/heap.c
@@ -135,6 +135,26 @@ void *heap_malloc_blocks(struct heap *heap, uint32_t total_blocks)
 out:
   return address;
 }
+
+void heap_mark_blocks_free(struct heap *heap, int starting_block)
+{
+  struct heap_table *table = heap->table;
+  for (int i = starting_block; i < (int)table->total; i++)
+  {
+    HEAP_BLOCK_TABLE_ENTRY entry = table->entries[i];
+    table->entries[i] = HEAP_BLOCK_TABLE_ENTRY_FREE;
+    if (!(entry & HEAP_BLOCK_HAS_NEXT))
+    {
+      break;
+    }
+  }
+}
+
+int heap_address_to_block(struct heap *heap, void *address)
+{
+  return ((int)(address - heap->start_addr)) / PEACHOS_HEAP_BLOCK_SIZE;
+}
+
 void *heap_malloc(struct heap *heap, size_t size)
 {
   size_t aligned_size = heap_align_value_to_upper(size);
@@ -144,5 +164,5 @@ void *heap_malloc(struct heap *heap, size_t size)
 
 void heap_free(struct heap *heap, void *ptr)
 {
-  // return 0;
+  heap_mark_blocks_free(heap, heap_address_to_block(heap, ptr));
 }
