f99dcad4847e1103c42cc36705fe7ac86a83545d
devj <power3xz@gmail.com>
2025-02-21 12:18:59 +0900

    streamer 추가

--- src/kernel.c
+++ src/kernel.c
@@ -9,6 +9,7 @@
 #include "string/string.h"
 #include "disk/disk.h"
 #include "fs/pparser.h"
+#include "disk/streamer.h"
 
 uint16_t *video_memory = 0;
 uint16_t terminal_row = 0;
@@ -90,8 +91,11 @@ void kernel_main()
   // enable the system interrupts
   enable_interrupts();
 
-  struct path_root *root_path = pathparser_parse("0:/bin/shell.exe", NULL);
-  if (root_path)
+  struct disk_stream *stream = diskstreamer_new(0);
+  diskstreamer_seek(stream, 0x201);
+  unsigned char c = 0;
+  diskstreamer_read(stream, &c, 1);
+  while (1)
   {
   }
 }
