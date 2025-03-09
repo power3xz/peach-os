b1649600c03f7fc89ffce66770efda949b7b56c9
devj <power3xz@gmail.com>
2025-03-09 12:57:11 +0900

    fstat 추가

--- src/kernel.c
+++ src/kernel.c
@@ -99,12 +99,8 @@ void kernel_main()
   int fd = fopen("0:/hello.txt", "r");
   if (fd)
   {
-    print("\nwe opened hello.txt\n");
-    char buf[14];
-    fseek(fd, 2, SEEK_SET);
-    fread(buf, 11, 1, fd);
-    buf[13] = 0x00;
-    print(buf);
+    struct file_stat s;
+    fstat(fd, &s);
   }
   else
   {
