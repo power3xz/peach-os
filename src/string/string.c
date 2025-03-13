ea486a010739928ecd702eed6ca4601daa8b05b4
devj <power3xz@gmail.com>
2025-03-13 13:06:57 +0900

    버그 수정 및 빌드 수정

--- src/string/string.c
+++ src/string/string.c
@@ -58,7 +58,7 @@ char *strcpy(char *dest, const char *src)
 char *strncpy(char *dest, const char *src, int count)
 {
   int i = 0;
-  for (int i = 0; i < count - 1; i++)
+  for (i = 0; i < count - 1; i++)
   {
     if (src[i] == 0x00)
     {
