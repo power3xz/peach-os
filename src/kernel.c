b5e62ceafd3a7dc9341907ac87cbb8190e06061b
devj <power3xz@gmail.com>
2025-03-20 15:43:30 +0900

    keyboard init 추가

--- src/kernel.c
+++ src/kernel.c
@@ -20,6 +20,7 @@
 #include "task/process.h"
 #include "status.h"
 #include "isr80h/isr80h.h"
+#include "keyboard/keyboard.h"
 
 uint16_t *video_memory = 0;
 uint16_t terminal_row = 0;
@@ -138,6 +139,8 @@ void kernel_main()
   enable_paging();
 
   isr80h_register_commands();
+
+  keyboard_init();
 
   struct process *process = 0;
   int res = process_load("0:/blank.bin", &process);
