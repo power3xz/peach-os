f2f3b939afc2a759570b0e8ed41d053dae1ddfeb
devj <power3xz@gmail.com>
2025-03-21 15:43:37 +0900

    키보드 입력 루프

--- programs/blank/blank.asm
+++ programs/blank/blank.asm
@@ -5,13 +5,13 @@ section .asm
 global _start
 
 _start:
+_loop:
   call getkey
   push eax
   mov eax, 3 ; command putchar
   int 0x80
   add esp, 4
-  jmp $
-
+  jmp _loop
 getkey:
   mov eax, 2 ; command getkey
   int 0x80
