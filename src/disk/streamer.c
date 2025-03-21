f29be5951ddf46f1a72cefb4033e6c16656095f6
devj <power3xz@gmail.com>
2025-03-21 15:53:14 +0900

    백스페이스 처리

--- src/disk/streamer.c
+++ src/disk/streamer.c
@@ -1,6 +1,7 @@
 #include "streamer.h"
 #include "memory/kheap.h"
 #include "config.h"
+#include <stdbool.h>
 
 struct disk_stream *diskstreamer_new(int disk_id)
 {
@@ -26,6 +27,8 @@ int diskstreamer_read(struct disk_stream *stream, void *out, int total)
 {
   int sector = stream->pos / PEACHOS_SECTOR_SIZE;
   int offset = stream->pos % PEACHOS_SECTOR_SIZE;
+  int total_to_read = total;
+  bool overflow = (offset + total_to_read) >= PEACHOS_SECTOR_SIZE;
   char buf[PEACHOS_SECTOR_SIZE];
 
   int res = disk_read_block(stream->disk, sector, 1, buf);
@@ -34,8 +37,7 @@ int diskstreamer_read(struct disk_stream *stream, void *out, int total)
     goto out;
   }
 
-  int total_to_read = total > PEACHOS_SECTOR_SIZE ? PEACHOS_SECTOR_SIZE : total;
-  for (int i = 0; i < total_to_read; i++)
+    for (int i = 0; i < total_to_read; i++)
   {
     *(char *)out++ = buf[offset + i];
   }
