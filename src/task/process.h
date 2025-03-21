dd06550518d70da36bef49ee21db73addd5dcba6
devj <power3xz@gmail.com>
2025-03-21 14:52:02 +0900

    키보드 입력 테스트

--- src/task/process.h
+++ src/task/process.h
@@ -33,6 +33,8 @@ struct process
     int head;
   } keyboard;
 };
+int process_switch(struct process *process);
+int process_load_switch(const char *filename, struct process **process);
 
 int process_load(const char *filename, struct process **process);
 int process_load_for_slot(const char *filename, struct process **processes, int process_slot);
