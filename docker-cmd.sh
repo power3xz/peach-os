73ba79e8d1cf9b7832b3351b8a9c2e97c9d2c6a0
devj <power3xz@gmail.com>
2025-04-10 15:56:07 +0900

    elf load 추가

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mount -t vfat ./os.bin /mnt/d
 cp ./hello.txt /mnt/d
-cp ./programs/blank/build/blank.bin /mnt/d/blank.bin
+cp ./programs/blank/build/blank.elf /mnt/d/blank.elf
 umount /mnt/d
