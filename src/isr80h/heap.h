4f38cf5e0f1d270040b1d18eb98af552d320fb48
devj <power3xz@gmail.com>
2025-04-14 16:35:49 +0900

    process malloc 추가

---
+++ src/isr80h/heap.h
@@ -0,0 +1,8 @@
+#ifndef ISH80H_HEAP_H
+#define ISH80H_HEAP_H
+
+struct interrupt_frame;
+
+void *isr80h_command4_malloc(struct interrupt_frame *frame);
+
+#endif
--- src/task/process.c
+++ src/task/process.c
@@ -286,3 +286,34 @@ out:
   }
   return res;
 }
+
+static int process_find_free_alocation_index(struct process *process)
+{
+  int res = -ENOMEM;
+  for (int i = 0; i < PEACHOS_MAX_PROGRAM_ALLOCATIONS; i++)
+  {
+    if (process->allocations[i] == 0)
+    {
+      res = i;
+      break;
+    }
+  }
+  return res;
+}
+
+void *process_malloc(struct process *process, size_t size)
+{
+  void *ptr = kzalloc(size);
+  if (!ptr)
+  {
+    return 0;
+  }
+
+  int index = process_find_free_alocation_index(process);
+  if (index < 0)
+  {
+    return 0;
+  }
+  process->allocations[index] = ptr;
+  return ptr;
+}
