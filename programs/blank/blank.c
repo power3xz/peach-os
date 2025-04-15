5f5bce62f9f1f4cfda23410c2954ea7e6b1d122c
devj <power3xz@gmail.com>
2025-04-16 00:55:29 +0900

    start 함수 추가

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -5,10 +5,7 @@
 
 int main(int argc, char **argv)
 {
-  struct process_arguments arguments;
-  peachos_process_get_arguments(&arguments);
-  printf("%i %s\n", arguments.argc, arguments.argv[0]);
-
+  print(argv[0]);
   while (1)
   {
   }
