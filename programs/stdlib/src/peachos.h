5225a2fc98d81aa7fb6e14530d3bd03a1b5f6ab7
devj <power3xz@gmail.com>
2025-04-16 00:03:08 +0900

    명령어 파싱 추가

--- programs/stdlib/src/peachos.h
+++ programs/stdlib/src/peachos.h
@@ -4,6 +4,12 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+struct command_argument
+{
+  char argument[512];
+  struct command_argument *next;
+};
+
 void print(const char *filename);
 int peachos_getkey();
 int peachos_getkey_block();
@@ -12,5 +18,6 @@ void peachos_free(void *ptr);
 void peachos_putchar(char c);
 void peachos_terminal_readline(char *out, int max, bool output_while_typing);
 void peachos_process_load_start(const char *filename);
+struct command_argument *peachos_parse_command(const char *command, int max);
 
 #endif
