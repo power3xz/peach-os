76d6fbaf73dd159879df1c59690f4e9f8e96fbe7
devj <power3xz@gmail.com>
2025-04-17 16:01:04 +0900

    exit 추가

--- programs/stdlib/src/peachos.asm
+++ programs/stdlib/src/peachos.asm
@@ -10,6 +10,7 @@ global peachos_putchar:function
 global peachos_process_load_start:function
 global peachos_process_get_arguments:function
 global peachos_system:function
+global peachos_exit:function
 
 ; void print(const char* filename)
 print:
@@ -108,5 +109,16 @@ peachos_process_get_arguments:
   int 0x80
 
   add esp, 4
+  pop ebp
+  ret
+
+; void peachos_exit()
+peachos_exit:
+  push ebp
+  mov ebp, esp
+
+  mov eax, 9 ; command exit
+  int 0x80
+
   pop ebp
   ret
