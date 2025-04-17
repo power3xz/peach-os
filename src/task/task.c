ca0197f193464ff53411d65c135dfa75bb12a0f0
devj <power3xz@gmail.com>
2025-04-17 15:45:02 +0900

    임시 커밋

--- src/task/task.c
+++ src/task/task.c
@@ -238,3 +238,15 @@ void *task_virtual_address_to_physical(struct task *task, void *virtual_address)
 {
   return paging_get_physical_address(task->page_directory->directory_entry, virtual_address);
 }
+
+void task_next()
+{
+  struct task *next_task = task_get_next();
+  if (!next_task)
+  {
+    panic("No more tasks!\n");
+  }
+
+  task_switch(next_task);
+  task_return(&next_task->registers);
+}
